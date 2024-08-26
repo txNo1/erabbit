01e92f02017f92d58a0f4f1a5a169eea0a51dcba
tx <576425427@qq.com>
2024-08-26 16:21:39 +0800

    feat: 请求和上传文件拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,29 +1,35 @@
 <script setup lang="ts">
-import '@/utils/http'
 import { useMemberStore } from '@/stores'
+import  '@/utils/http'
 
 const memberStore = useMemberStore()
+
+//测试请求
+const getDate = ()=>{
+  uni.request({
+    method:'GET',
+    url:'home/banner',
+    success:(res)=>{
+      console.log(res);
+    }
+  })
+}
 </script>
 
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-        })
-      "
-      size="mini"
-      plain
-      type="primary"
-    >
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋',
+      })
+      " size="mini" plain type="primary">
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getDate" size="mini" plain type="primary">测试数据</button>
   </view>
+
 </template>
 
-<style lang="scss">
-//
-</style>
+<style lang="scss"></style>
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,1 +1,30 @@
+import { useMemberStore } from '@/stores'
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 添加拦截器
+const httpInterceptor = {
+  invoke(options: UniApp.RequestOptions) {
+    // 1. 非http开头需要拼接地址
+    if (!options.url.startsWith('http')) {
+      options.url = `${baseURL}/${options.url}`; // 确保拼接正确
+    }
+    // 2. 超时时间，默认10s
+    options.timeout = 10000;
+    console.log('Request options:', options); // 打印请求选项以便调试
+
+    // 3.添加小程序端请求头标识
+    options.header = {
+      ...options.header,
+      'source': 'miniapp',
+    }
+    // 4.添加token 请求头标识
+    const memberStore = useMemberStore();
+    const token = memberStore.profile?.token;
+    if(token){
+      options.header.Authorization = token
+    }
+  },
+};
+
+uni.addInterceptor('request', httpInterceptor);
+uni.addInterceptor('uploadFile', httpInterceptor);
